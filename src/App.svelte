2b27e98bc25c5a1760aa0c984a56dd2b2bad3d31
엄진용 <wlsdyd7511@gmail.com>
2024-12-30 23:01:26 +0900

    Implement group and item

--- src/App.svelte
+++ src/App.svelte
@@ -10,8 +10,59 @@
         config.expenses = JSON.parse(localStorage.getItem("expenses"));
         config.configured = true;
     }
+
+    const groups = $state([]);
+    let selectedGroupIdx = $state(0);
+
+    function addGroup() {
+        groups.push({
+            name: `${groups.length + 1}`,
+            checked: [],
+            items: [],
+        });
+        selectedGroupIdx = groups.length - 1;
+    }
+
+    function addItem() {
+        groups[selectedGroupIdx].items.push({
+            name: groups[selectedGroupIdx].items.length + 1,
+            cost: 0,
+        });
+    }
 </script>
 
 <main>
     <Config />
+    <table border="1">
+        <tbody>
+            <tr>
+                <th colspan="2"> </th>
+                <th> </th>
+            </tr>
+
+            {#each groups as group, idx}
+                <tr>
+                    <td colspan="2"> {group.name} </td>
+                    <td> </td>
+                </tr>
+                {#each group.items as item, idx}
+                    <tr>
+                        <td colspan="2"> {item.name} </td>
+                        <td>
+                            <input type="number" bind:value={item.cost} />
+                        </td>
+                    </tr>
+                {/each}
+            {/each}
+
+            <tr>
+                <td>
+                    <button onclick={addGroup}> 그룹+ </button>
+                </td>
+                <td>
+                    <button onclick={addItem}> 항목+ </button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>

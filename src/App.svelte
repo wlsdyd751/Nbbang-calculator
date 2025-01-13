4715f83dbefd6fc674411b7c8a9200dae02e9958
엄진용 <wlsdyd7511@gmail.com>
2025-01-13 23:47:10 +0900

    Improve division and implement webhook submit
    Improve division to make integer result

--- src/App.svelte
+++ src/App.svelte
@@ -8,11 +8,20 @@
         config.bond = localStorage.getItem("bond");
         config.debtors = JSON.parse(localStorage.getItem("debtors"));
         config.expenses = JSON.parse(localStorage.getItem("expenses"));
+        config.credits = JSON.parse(localStorage.getItem("credits"));
         config.configured = true;
     }
 
     let people = $state([]);
     let N = $state(1);
+
+    let selectedCredit = $state("");
+    let selectedExpense = $state("");
+    let item = $state("");
+    let today = new Date();
+    let dateString = $state("");
+    let date = $derived(dateString.replace(/-/g, ""));
+    let memo = $state("");
 
     const groups = $state([]);
     let selectedGroupIdx = $state(0);
@@ -22,24 +31,40 @@
     let selectedCnt = $derived(
         groups.map((g) => g.checked.reduce((acc, c) => acc + (c ? 1 : 0), 0)),
     );
-    let personalTotal = $derived.by(() => {
-        let res = [];
-        console.log(subtotals);
-        console.log(selectedCnt);
+    let { personalTotal, actualTotal } = $derived.by(() => {
+        let p = [];
+        let a = [];
         for (let i = 0; i < N; i++) {
-            let total = 0;
-            for (let gi = 0; gi < groups.length; gi++) {
-                if (!groups[gi].checked[i]) continue;
-
-                total += subtotals[gi] / selectedCnt[gi];
-            }
-            res.push(total);
+            p.push(0);
+            a.push(0);
         }
-        return res;
+        for (let [gi, group] of groups.entries()) {
+            for (let i = 0; i < N; i++) {
+                if (!group.checked[i]) continue;
+                a[i] += subtotals[gi] / selectedCnt[gi];
+                p[i] += Math.floor(subtotals[gi] / selectedCnt[gi]);
+            }
+
+            let indexes = [];
+            for (let i = 0; i < N; i++) {
+                indexes.push(i);
+            }
+            console.log("idxx");
+            console.log(indexes);
+            indexes.sort((i, j) => {
+                return p[i] - a[i] - p[j] + a[j];
+            });
+            console.log("idxsorted");
+            console.log(indexes);
+
+            for (let i = 0; i < subtotals[gi] % selectedCnt[gi]; i++) {
+                p[indexes[i]] += 1;
+            }
+        }
+        return { personalTotal: p, actualTotal: a };
     });
 
     $inspect(personalTotal);
-    $inspect(groups);
 
     function addGroup() {
         groups.push({
@@ -85,17 +110,73 @@
         }
         e.target.value = N;
     }
+
+    function submitToWhooing() {
+        for (let i = 0; i < N; i++) {
+            if (personalTotal[i] > 0) {
+                let left;
+                let right = selectedCredit;
+                let dataItem = item;
+                if (i == 0) {
+                    left = selectedExpense;
+                } else {
+                    let name = people[i - 1];
+                    let debit = config.debtors.find(
+                        (d) => d.name === name,
+                    ).debit;
+                    console.log(`debit: //${debit}//`);
+
+                    if (debit === "") {
+                        left = config.bond;
+                        dataItem = name;
+                    } else {
+                        left = debit;
+                    }
+                }
+                const data = {
+                    entry_date: parseInt(date),
+                    item: dataItem,
+                    money: personalTotal[i],
+                    left,
+                    right,
+                    memo,
+                };
+                console.log("data");
+                console.log(JSON.stringify(data));
+
+                fetch(config.endpoint, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify(data),
+                });
+            }
+        }
+    }
 </script>
 
 <main>
     <Config />
-    <label for="N">인원수</label>
+    <label for="N"> 인원수 </label>
     <input type="number" id="N" onchange={NChange} defaultValue="1" />
+    <br />
+    왼쪽
+    <Dropdown bind:value={selectedExpense} options={config.expenses} />
+    오른쪽
+    <Dropdown bind:value={selectedCredit} options={config.credits} />
+    <br />
+    <label for="item"> 아이템 </label>
+    <input type="text" id="item" placeholder="아이템" bind:value={item} />
+    <br />
+    <label for="date"> 날짜 </label>
+    <input type="date" id="date" bind:value={dateString} />
+    <br />
+    <label for="memo"> 메모 </label>
+    <input type="text" id="memo" placeholder="메모" bind:value={memo} />
     <table border="1">
         <tbody>
             <tr>
                 <th colspan="2"> </th>
-                <th> me </th>
+                <th> 나 </th>
                 {#each { length: N - 1 }, i}
                     <th>
                         <Dropdown
@@ -169,7 +250,13 @@
                     <td> {personalTotal[i]} </td>
                 {/each}
             </tr>
+            <tr>
+                <td colspan="2"> diff </td>
+                {#each { length: N }, i}
+                    <td> {(personalTotal[i] - actualTotal[i]).toFixed(2)} </td>
+                {/each}
+            </tr>
         </tbody>
     </table>
-    <button> 후잉 업로드 </button>
+    <button onclick={submitToWhooing}> 후잉 업로드 </button>
 </main>
--- src/config.svelte.js
+++ src/config.svelte.js
@@ -5,4 +5,5 @@ export const config = $state({
   bond: "",
   debtors: [],
   expenses: [],
+  credits: [],
 });
